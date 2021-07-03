5f9f01aab77243cf3c1d4365a1f3d4fc69b8edf7
Icarus <1375400884@qq.com>
2021-07-04 03:29:32 +0800

    !A(Options) KeywordType

---
+++ src/CSharpToTypeScript.Core/Options/KeywordType.cs
@@ -0,0 +1,14 @@
+//Create: Icarus
+//ヾ(•ω•`)o
+//2021-07-04 02:16
+//CSharpToTypeScript.Core
+
+namespace CSharpToTypeScript.Core.Options
+{
+    public enum KeywordType
+    {
+        Private,
+        Export,
+        Declare
+    }
+}
